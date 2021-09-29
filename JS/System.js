2e92804b8eef59ea0ff4f69080586f76c4690257
Sx-Cheats <65744142+Sx-Cheats@users.noreply.github.com>
2021-09-29 10:48:26 +0200

    Update System.js

--- JS/System.js
+++ JS/System.js
@@ -38,7 +38,6 @@ function MATRICE1D_TO_3D(M) {
 
 }
 function CHECK_MATRICE(M) {
-    console.log(M)
     if (!(((M[0].reduce(Sum) ^ 3) * (M[1].reduce(Sum) ^ 3) * (M[2].reduce(Sum) ^ 3)) * (((M[0][0] + M[1][0] + M[2][0]) ^ 3) * ((M[0][1] + M[1][1] + M[2][1]) ^ 3) * ((M[0][2] + M[1][2] + M[2][2]) ^ 3)) * ((M[0][0] + M[1][1] + M[2][2]) ^ 3) * ((M[0][2] + M[1][1] + M[2][0]) ^ 3))) {
         return true
     }
